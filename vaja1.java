8d7956d0f432612f8fd3f932e2b7435335246aac
Mihajlo Ivanović <mihajlo.ivanovic@scng.si>
2026-09-09 09:19:57 +0200

    Je

--- vaja1.java
+++ vaja1.java
@@ -1,5 +1,16 @@
 public class vaja1{
     public static void main(String[] args){
-        System.out.println("Rad imam Javo");
+        System.out.println("2 na 10 = "+potenca(10));
+        System.out.println("7 fib stevilo je "+fib(7));
+    }
+    public static int potenca (int n){
+        if (n==0)
+            return 1;
+        return 2*potenca(n-1);
+    }
+    public static int fib(int n){
+        if(n==1||n==2)
+            return 1;
+        return fib(n-1)+fib(n-2);
     }
 }
